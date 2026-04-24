5caadeeac315914d86037a4c73be6cd42b218cea
Sahil-Lakhani <sahillakhani266@gmail.com>
2026-04-24 21:15:03 +0200

    added routes for calculator

--- NewGen/src/App.jsx
+++ NewGen/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
+import Calculator from './components/Calculator';
 
 function App() {
-  return <Landing />;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
+  );
 }
 
 export default App;
